82e910e6d923d58cded8293d39e974fee30b0cfd
Eugene T <tyanbz@gmail.com>
2023-10-03 14:18:51 +0600

    lesson finished

---
+++ tasks. part 1/task 6/m1l3t6.py
@@ -0,0 +1,24 @@
+'''
+Задача 6 (Напиши программу)
+
+Бо слушает свою любимую песню в Spotify и хочет создать караоке. Помоги Бо написать программу, которая будет выводить текст песни построчно.
+
+Текст песни:
+
+I wish I didn't have to rhyme every time I sang
+I was told when I get older, all my fears would shrink
+But now I'm insecure, and I care what people think
+My name's Blurryface and I care what you think
+Последняя строчка повторяется два раза.
+'''
+
+# Индексы строк песни:
+#  :47
+# 48:103
+# 103:154
+# 154:
+
+# твой код
+
+song = "I wish I didn't have to rhyme every time I sang I was told when I get older, all my fears would shrink But now I'm insecure, and I care what people think My name's Blurryface and I care what you think"
+
